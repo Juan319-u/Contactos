c140fc78fbc75bd060a771e5ef3cf5e383f69aba
Juan319-u <80417972+Juan319-u@users.noreply.github.com>
2021-10-12 21:40:19 -0500

    Update contactos.py

--- contactos.py
+++ contactos.py
@@ -3,16 +3,27 @@ Autor : Juan  Felipe  Corrales  Toro
 ULTIMA  ACTUALIZACION : 04  de  Octubre  /  2021'''
 contactos={}#abrimos una diccionario vacio
 opciones=""
+
 while opciones !="4":#usamos un while para que la "app" de contactos se cierre cuando el usuario lo pida
     opciones=input("menu\n (1)agregar\n (2)mostrar\n (3)eliminar\n (4)terminar\n")#menu
+    
+    
     if opciones =="1":
         nombre =input("ingrese el nombre del contacto ")
+        while nombre=="":
+            print("no puede estar vacio")
+            nombre =input("ingrese el nombre del contacto ")
         if nombre in contactos:
             print("este contacto ya existe")
         else:
             telefono =input("ingrese el numero del contacto ")
+            while telefono=="":
+                print("no puede estar vacio")
+                telefono =input("ingrese el numero del contacto ")
             contactos[nombre]=int(telefono)#ingresamos el resultado al diccionario
             print("se guardo con exito el contacto de ",nombre)
+            
+            
     if opciones =="2":
         verificaciones =input("desea ver toda la lista de contactos si/no? ")
         if verificaciones == "si":
@@ -23,6 +34,8 @@ while opciones !="4":#usamos un while para que la "app" de contactos se cierre c
                 print(nombre, ":",contactos[nombre])
             else:
                 print("este contacto no existe ")
+                
+                
     if opciones=="3":
             nombre=input("ingrese el nombre del usuario que desea eliminar ")
             if nombre in contactos:
